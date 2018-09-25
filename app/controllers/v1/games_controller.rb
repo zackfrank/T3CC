b0b6b9533d2a9df033b3cd17824e42234d41dbce
Zack Frank <frank.zack@gmail.com>
2018-09-25 12:54:22 -0500

    Fix games controller, had tried something before previous commit but needed to switch back which I did for this commit

--- app/controllers/v1/games_controller.rb
+++ app/controllers/v1/games_controller.rb
@@ -1,11 +1,8 @@
 class V1::GamesController < ApplicationController
 
   def create
-    # game = Game.create()
-    # game.setup(params)
-
-    game = params[:game]
-    game.udpate(params)
+    game = Game.create()
+    game.setup(params)
 
     render json: game.as_json
   end
